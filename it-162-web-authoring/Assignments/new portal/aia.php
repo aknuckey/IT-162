e46988c76e0cd3b94287d660016078873a8c042b
aknuckey <74021230+aknuckey@users.noreply.github.com>
2021-04-22 09:54:52 -0700

    Add files via upload

---
+++ it-162-web-authoring/Assignments/new portal/aia.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php'?>
+
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">AIA</h2>
+
+ <img src="images/desktop.jpg" class="desktop" alt="salon" />
+ <img src="images/tablet.jpg" class="tablet" alt="shop" />
+ <img src="images/phone.jpg" class="phone" alt="sunset" />
+
+ 	<p class="about">We'll get there when we get there</p>
+	
+</section>
+<!-- END LEFT COL -->
+<aside>
+	<p class="tababout">I'm not working on it... I'm enjoying the sun...</p>
+</aside>
+
+<?php include 'includes/footer.php'?>
---
+++ it-162-web-authoring/Assignments/new portal/contactme.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php'?>
+
+<?php include 'includes/contact_include.php'?>
+
+<?php include 'includes/multiple.php'?>
+
+<?php include 'includes/footer.php'?>
