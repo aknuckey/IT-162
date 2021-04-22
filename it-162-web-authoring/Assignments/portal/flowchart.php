dc9c3aaa59b6ff248103290b5056f2f1f6c9298d
aknuckey <74021230+aknuckey@users.noreply.github.com>
2021-04-22 09:55:51 -0700

    Add files via upload

---
+++ it-162-web-authoring/Assignments/portal/flowchart.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php'?>
+
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Flowchart/Layout</h2>
+
+ <img src="images/desktop.jpg" class="desktop" alt="salon" />
+ <img src="images/tablet.jpg" class="tablet" alt="shop" />
+ <img src="images/phone.jpg" class="phone" alt="sunset" />
+
+ 	<p class="about">Workin' on it...</p>
+	
+</section>
+<!-- END LEFT COL -->
+<aside>
+	<p class="tababout">Gettin' there...</p>
+</aside>
+
+<?php include 'includes/footer.php'?>
---
+++ it-162-web-authoring/Assignments/portal/index.php
@@ -0,0 +1,23 @@
+<?php include 'includes/header.php'?>
+
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+ <img src="images/desktop.jpg" class="desktop" alt="salon" />
+ <img src="images/tablet.jpg" class="tablet" alt="shop" />
+ <img src="images/phone.jpg" class="phone" alt="sunset" />
+
+ 	<p class="about">I’ve been a Barber for the last 13 years. I love working with people and helping to bring out the best in them. That career is coming to an end for me and I hope to build a new one within the Web Development field as a Project Manager.</p>
+    <p class="about">Born and Raised in Seattle, when I’m not working or studying, I enjoy spending time in the beautiful mountains that surround us and working with my hands. Fixing up our house, working on cars, playing with the dogs, hiking, hunting, snowboarding, exercising, and spending time with friends and family.</p> 
+    <p class="about">I look forward to what the future holds and I hope to have fun on the way there!</p>
+	
+</section>
+<!-- END LEFT COL -->
+<aside>
+	<p class="tababout">I’ve been a Barber for the last 13 years. I love working with people and helping to bring out the best in them. That career is coming to an end for me and I hope to build a new one within the Web Development field as a Project Manager.</p>
+    <p class="tababout">Born and Raised in Seattle, when I’m not working or studying, I enjoy spending time in the beautiful mountains that surround us and working with my hands. Fixing up our house, working on cars, playing with the dogs, hiking, hunting, snowboarding, exercising, and spending time with friends and family.</p> 
+    <p class="tababout">I look forward to what the future holds and I hope to have fun on the way there!</p>
+</aside>
+
+<?php include 'includes/footer.php'?>
