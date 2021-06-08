9c2edbe36cc76a0adf56e9ad59e2bbd2e55304d5
aknuckey <74021230+aknuckey@users.noreply.github.com>
2021-06-08 16:45:12 -0700

    Add files via upload

--- template.php/index.php
+++ template.php/index.php
@@ -1,60 +1,16 @@
 <?php include 'includes/header.php'?>
 <link rel="stylesheet" type="text/css" href='css/index.css' />
-        <div class="container">
-            <div class="index screen">
-                <div class="flex-row">
-                    <div class="news-1">
-                        <img class="main-headline-img" src="img/death-star@1x.png" />
-                        <img class="main-label" src="img/label@1x.png" />
-                        <p class="main-headline arial-regular-normal-white-14px">
-                            New Star Wars Death Star rumored to be the largest LEGO™ set ever
-                        </p>
-                        <div class="main-headline-title arial-bold-white-20px">Coming Soon!!!</div>
-                    </div>
-                    <div class="flex-col-grid">
-                        <div class="flex-row-top">
-                            <div class="news-2">
-                                <img class="top-center-img" src="img/quanta-ta@1x.png" />
-                                <img class="top-center-label" src="img/label-3@1x.png" />
-                                <p class="top-center-des arial-regular-normal-white-14px">
-                                    Check out what’s new with us here at Math ’n’ Stuff™
-                                </p>
-                                <div class="top-center-title arial-bold-white-20px">Whats New</div>
-                            </div>
-                            <div class="news">
-                                <img class="top-left-img" src="img/mapple-leaf-park@1x.png" />
-                                <img class="top-left-label" src="img/label-2@1x.png" />
-                                <p class="top-left-des arial-regular-normal-white-14px">
-                                    Keep up to date with Maple Leaf news
-                                </p>
-                                <div class="top-left-title arial-bold-white-20px">Neighborhood News</div>
-                            </div>
-                        </div>
-                        <div class="flex-row-bottom">
-                            <div class="news-3">
-                                <img class="bottom-center-right-img" src="img/pokemon-players-cup@1x.png" />
-                                <img class="bottom-center-right-label" src="img/label-2@1x.png" />
-                                <div class="bottom-center-right-des arial-regular-normal-white-14px">Begins April 2021</div>
-                                <div class="bottom-center-right-title arial-bold-white-20px">Pokémon™ Players Cup IV</div>
-                            </div>
-                            <div class="news">
-                                <img class="bottom-center-left-img" src="img/board-games@1x.png" />
-                                <img class="bottom-center-left-label" src="img/label-2@1x.png" />
-                                <div class="bottom-center-left-des arial-regular-normal-white-14px">Our recommendations</div>
-                                <div class="bottom-center-left-title arial-bold-white-20px">Reconnect with Board Games</div>
-                            </div>
-                            <div class="news">
-                                <img class="bottom-left-img" src="img/irian-strelnikova@1x.png" />
-                                <img class="bottom-left-label" src="img/label-2@1x.png" />
-                                <div class="credit-img">- Irian Strelnikova/iStock/Getty</div>
-                                <p class="bottom-left-des arial-regular-normal-white-14px">
-                                    We want to connect you and local educators
-                                </p>
-                                <div class="bottom-left-title arial-bold-white-20px">Find Tutors</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<body>
+    <div class="row"> 
+        <div class="column">
+          <img class="one" src="images/death-star@1x.png" style="width:100%">
         </div>
+        <div class="column">
+            <img class="two" src="images/monopoly@1x.png" style="width:100%">
+            <img class="three" src="images/quanta_ta.png" style="width:100%">
+          </div> 
+          <div class="column">
+            <img class="four" src="images/math-justice-b.png" style="width:100%">
+            <img class="four" src="images/mapple-leaf-park@1x.png" style="width:100%">
+      </div>
 <?php include 'includes/footer.php'?>
